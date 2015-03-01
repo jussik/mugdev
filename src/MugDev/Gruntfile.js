1e9aa245b19df928522008a426f0b77f922510d5
Jussi Kosunen <deathcarrot@gmail.com>
2015-03-01 11:22:20 +0200

    separate grunt task for debug

--- src/MugDev/Gruntfile.js
+++ src/MugDev/Gruntfile.js
@@ -1,29 +1,45 @@
 ﻿module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-ng-annotate');
-    grunt.loadNpmTasks('grunt-contrib-less');
 
     grunt.initConfig({
+        clean: ['wwwroot/js', 'wwwroot/css', 'Build'],
+        concat: {
+            default: {
+                options: { sourceMap: true },
+                files: { 'wwwroot/js/app.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
+            }
+        },
         ngAnnotate: {
-            mugs: {
-                files: {
-                    'Build/js/app.annotated.js': ['Scripts/app.js', 'Scripts/**/*.js']
-                }
+            default: {
+                files: { 'Build/js/app.annotated.js': ['Scripts/app.js', 'Scripts/**/*.js'] }
             }
         },
         uglify: {
-            mugs: {
+            default: {
                 files: { 'wwwroot/js/app.js': ['Build/js/app.annotated.js'] }
             }
         },
         less: {
-            styles: {
+            release: {
                 options: { compress: true, ieCompat: false },
+                files: { 'wwwroot/css/app.css': ['Styles/**.less'] }
+            },
+            debug: {
+                options: { sourceMap: true, ieCompat: false },
                 files: { 'wwwroot/css/app.css': ['Styles/**.less'] }
             }
         },
         watch: {
+            concat: {
+                files: ['Scripts/**/*.js'],
+                tasks: ['concat']
+            },
             annotate: {
                 files: ['Scripts/**/*.js'],
                 tasks: ['ngAnnotate']
@@ -32,12 +48,28 @@
                 files: ['Build/js/app.annotated.js'],
                 tasks: ['uglify']
             },
-            less: {
+            lessRelease: {
                 files: ['Styles/**.less'],
-                tasks: ['less']
+                tasks: ['less:release']
+            },
+            lessDebug: {
+                files: ['Styles/**.less'],
+                tasks: ['less:debug']
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            release: {
+                tasks: ["watch:annotate", "watch:lessRelease"]
+            },
+            debug: {
+                tasks: ["watch:concat", "watch:lessDebug"]
             }
         }
     });
 
-    grunt.registerTask('default', ['ngAnnotate', 'uglify', 'less', 'watch']);
+    grunt.registerTask('release', ['clean', 'ngAnnotate', 'uglify', 'less:release', 'concurrent:release']);
+    grunt.registerTask('debug', ['clean', 'concat', 'less:debug', 'concurrent:debug']);
 };
